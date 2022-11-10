88ae46b7645ef24a0f7fcf89e945f6370cae88a7
mohammad <pcadocument@gmail.com>
2022-11-10 19:27:00 +0330

    Add project files.

---
+++ pg6/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace pg6
+{
+    class Program
+    {
+        private static void Main()
+        {
+            string str = Console.ReadLine();
+            int age = Convert.ToInt32(str);
+            Console.WriteLine(calculate(num));
+        }
+        public static int calculate
+    }
+}
